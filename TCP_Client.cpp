a177873f319effddf9865067d51497c34404fc1b
loopgad <3280646246@qq.com>
2024-10-30 20:56:46 +0800

    update

--- TCP_Client.cpp
+++ TCP_Client.cpp
@@ -1,86 +1,79 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <signal.h>
-#include <arpa/inet.h>
+#include <stdio.h>                      // 标准输入输出库
+#include <sys/types.h>                 // 类型定义
+#include <sys/socket.h>                // 套接字库
+#include <errno.h>                     // 错误号库
+#include <string.h>                    // 字符串处理库
+#include <stdlib.h>                    // 标准库
+#include <unistd.h>                    // Unix标准函数库
+#include <netinet/in.h>                // Internet地址族
+#include <signal.h>                    // 信号处理库
+#include <arpa/inet.h>                 // 网络地址转换库
 
-#define MAXSIZE 1024
-#define IP_ADDR "127.0.0.1"
-#define IP_PORT 7878
+#define MAXSIZE 1024                   // 定义最大数据长度常量
+#define IP_ADDR "127.0.0.1"            // 定义服务器IP地址
+#define IP_PORT 7878                   // 定义服务器端口号
 
-int i_sockfd = -1;
+int i_sockfd = -1;                     // 定义套接字文件描述符，并初始化为-1表示未创建
 
-void SigCatch(int sigNum)	//信号捕捉函数(捕获Ctrl+C)
-{
-	if(i_sockfd != -1)
-	{
-		close(i_sockfd);
-	}
-	printf("Bye~! Will Exit...\n");
-	exit(0);
+// 信号捕捉函数，用于捕获Ctrl+C信号
+void SigCatch(int sigNum) {
+    if(i_sockfd != -1) {
+        close(i_sockfd);               // 如果套接字已创建，则关闭套接字
+    }
+    printf("Bye~! Will Exit...\n");    // 打印退出信息
+    exit(0);                           // 退出程序
 }
 
-int main()
-{
-	struct sockaddr_in st_clnsock;
-	char msg[1024];
-	int nrecvSize = 0;
+int main() {
+    struct sockaddr_in st_clnsock;     // 定义套接字地址结构体
+    char msg[MAXSIZE];                 // 定义消息缓冲区
+    int nrecvSize = 0;                 // 定义接收数据大小变量
 
-	signal(SIGINT, SigCatch);	//注册信号捕获函数
+    // 注册信号捕获函数，当接收到SIGINT信号时调用SigCatch函数
+    signal(SIGINT, SigCatch);
 
-	if((i_sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0)	//建立套接字
-	{
-		printf("socket Error: %s (errno: %d)\n", strerror(errno), errno);
-		exit(0);
-	}
+    // 创建套接字，AF_INET表示IPv4，SOCK_STREAM表示TCP
+    if((i_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("socket Error: %s (errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
 
-	memset(&st_clnsock, 0, sizeof(st_clnsock));
-	st_clnsock.sin_family = AF_INET;  //IPv4协议
-	//IP地址转换(直接可以从物理字节序的点分十进制 转换成网络字节序)
-	if(inet_pton(AF_INET, IP_ADDR, &st_clnsock.sin_addr) <= 0)
-	{
-		printf("inet_pton Error: %s (errno: %d)\n", strerror(errno), errno);
-		exit(0);
-	}
-	st_clnsock.sin_port = htons(IP_PORT);	//端口转换(物理字节序到网络字节序)
+    // 清空地址结构体
+    memset(&st_clnsock, 0, sizeof(st_clnsock));
+    st_clnsock.sin_family = AF_INET;    // 设置地址族为IPv4
+    // 将IP地址从点分十进制转换为网络字节序
+    if(inet_pton(AF_INET, IP_ADDR, &st_clnsock.sin_addr) <= 0) {
+        printf("inet_pton Error: %s (errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
+    st_clnsock.sin_port = htons(IP_PORT); // 将端口号转换为网络字节序
 
-	if(connect(i_sockfd, (struct sockaddr*)&st_clnsock, sizeof(st_clnsock)) < 0)	//主动向设置的IP和端口号的服务端发出连接
-	{
-		printf("connect Error: %s (errno: %d)\n", strerror(errno), errno);
-		exit(0);
-	}
+    // 向服务器的IP地址和端口号发起连接请求
+    if(connect(i_sockfd, (struct sockaddr*)&st_clnsock, sizeof(st_clnsock)) < 0) {
+        printf("connect Error: %s (errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
 
-	printf("======connect to server, sent data======\n");
+    printf("======connect to server, sent data======\n");
 
-	while(1)	//循环输入，向服务端发送数据并接受服务端返回的数据
-	{
-		fgets(msg, MAXSIZE, stdin);
-		printf("will send: %s", msg);
-		if(write(i_sockfd, msg, MAXSIZE) < 0)	//发送数据
-		{
-			printf("write Error: %s (errno: %d)\n", strerror(errno), errno);
-			exit(0);
-		}
+    while(1) { // 无限循环，持续与服务器进行数据交换
+        fgets(msg, MAXSIZE, stdin);     // 从标准输入读取数据
+        printf("will send: %s", msg);     // 打印即将发送的数据
+        // 向服务器发送数据
+        if(write(i_sockfd, msg, MAXSIZE) < 0) {
+            printf("write Error: %s (errno: %d)\n", strerror(errno), errno);
+            exit(0);
+        }
 
-		memset(msg, 0, sizeof(msg));
-		if((nrecvSize = read(i_sockfd, msg, MAXSIZE)) < 0)	//接受数据
-		{
-			printf("read Error: %s (errno: %d)\n", strerror(errno), errno);
-		}
-		else if(nrecvSize == 0)
-		{
-			printf("Service Close!\n");
-		}
-		else
-		{
-			printf("Server return: %s\n", msg);
-		}
-
-	}
-	return 0;
+        memset(msg, 0, sizeof(msg));     // 清空消息缓冲区
+        // 从服务器接收数据
+        if((nrecvSize = read(i_sockfd, msg, MAXSIZE)) < 0) {
+            printf("read Error: %s (errno: %d)\n", strerror(errno), errno);
+        } else if(nrecvSize == 0) {
+            printf("Service Close!\n");   // 服务器关闭连接
+        } else {
+            printf("Server return: %s\n", msg); // 打印从服务器接收到的数据
+        }
+    }
+    return 0; // 程序正常退出
 }
--- TCP_Server.cpp
+++ TCP_Server.cpp
@@ -1,92 +1,84 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <ctype.h>
+#include <stdio.h>                      // 标准输入输出库
+#include <sys/types.h>                 // 类型定义
+#include <sys/socket.h>                // 套接字库
+#include <errno.h>                     // 错误号库
+#include <string.h>                    // 字符串处理库
+#include <stdlib.h>                    // 标准库
+#include <unistd.h>                    // Unix标准函数库
+#include <netinet/in.h>                // Internet地址族
+#include <ctype.h>                     // 字符类型库，用于字符转换
 
-#define MAXSIZE 1024
-#define IP_ADDR "127.0.0.1"
-#define IP_PORT 8888
+#define MAXSIZE 1024                   // 定义最大数据长度常量
+#define IP_ADDR "127.0.0.1"            // 定义服务器IP地址
+#define IP_PORT 8888                   // 定义服务器端口号
 
 int main()
 {
-	int i_listenfd, i_connfd;
-	struct sockaddr_in st_sersock;
-	char msg[MAXSIZE];
-	int nrecvSize = 0;
+    int i_listenfd, i_connfd;           // 监听套接字和连接套接字文件描述符
+    struct sockaddr_in st_sersock;     // 定义套接字地址结构体
+    char msg[MAXSIZE];                 // 定义消息缓冲区
+    int nrecvSize = 0;                 // 定义接收数据大小变量
 
-	if((i_listenfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0)	//建立socket套接字
-	{
-		printf("socket Error: %s (errno: %d)\n", strerror(errno), errno);
-		exit(0);
-	}
+    // 创建IPv4 TCP套接字
+    if ((i_listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("socket Error: %s (errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
 
-	memset(&st_sersock, 0, sizeof(st_sersock));
-	st_sersock.sin_family = AF_INET;  //IPv4协议
-	st_sersock.sin_addr.s_addr = htonl(INADDR_ANY);	//INADDR_ANY转换过来就是0.0.0.0，泛指本机的意思，也就是表示本机的所有IP，因为有些机子不止一块网卡，多网卡的情况下，这个就表示所有网卡ip地址的意思。
-	st_sersock.sin_port = htons(IP_PORT);
+    // 清空地址结构体并设置IPv4地址族、任意IP地址和指定端口号
+    memset(&st_sersock, 0, sizeof(st_sersock));
+    st_sersock.sin_family = AF_INET;
+    st_sersock.sin_addr.s_addr = htonl(INADDR_ANY); // 监听所有网卡的指定端口
+    st_sersock.sin_port = htons(IP_PORT);
 
-	if(bind(i_listenfd,(struct sockaddr*)&st_sersock, sizeof(st_sersock)) < 0) //将套接字绑定IP和端口用于监听
-	{
-		printf("bind Error: %s (errno: %d)\n", strerror(errno), errno);
-		exit(0);
-	}
+    // 将套接字绑定到IP地址和端口上
+    if (bind(i_listenfd, (struct sockaddr*)&st_sersock, sizeof(st_sersock)) < 0) {
+        printf("bind Error: %s (errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
 
-	if(listen(i_listenfd, 20) < 0)	//设定可同时排队的客户端最大连接个数
-	{
-		printf("listen Error: %s (errno: %d)\n", strerror(errno), errno);
-		exit(0);
-	}
+    // 设置套接字为监听状态，允许2个客户端连接请求排队
+    if (listen(i_listenfd, 2) < 0) {
+        printf("listen Error: %s (errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
 
-	printf("======waiting for client's request======\n");
-	//准备接受客户端连接
-	{
-		if((i_connfd = accept(i_listenfd, (struct sockaddr*)NULL, NULL)) < 0)	//阻塞等待客户端连接
-		{
-			printf("accept Error: %s (errno: %d)\n", strerror(errno), errno);
-		//	continue;
-		}	
-		else
-		{
-			printf("Client[%d], welcome!\n", i_connfd);
-		}
+    printf("======waiting for client's request======\n");
 
+    // 接受客户端连接请求
+    if ((i_connfd = accept(i_listenfd, (struct sockaddr*)NULL, NULL)) < 0) {
+        printf("accept Error: %s (errno: %d)\n", strerror(errno), errno);
+    } else {
+        printf("Client[%d], welcome!\n", i_connfd);
+    }
 
-		while(1)	//循环 接受客户端发来的消息并作处理(小写转大写)后回写给客户端
-		{
-			memset(msg, 0 ,sizeof(msg));
-			if((nrecvSize = read(i_connfd, msg, MAXSIZE)) < 0)
-			{
-				printf("accept Error: %s (errno: %d)\n", strerror(errno), errno);
-				continue;
-			}
-			else if( nrecvSize == 0)	//read返回0代表对方已close断开连接。
-			{
-				printf("client has disconnected!\n");
-				close(i_connfd);  //
-				break;
-			}
-			else
-			{
-				printf("recvMsg:%s", msg);
-				for(int i=0; msg[i] != '\0'; i++)
-				{
-					msg[i] = toupper(msg[i]);
-				}
-				if(write(i_connfd, msg, strlen(msg)+1) < 0)
-				{
-					printf("accept Error: %s (errno: %d)\n", strerror(errno), errno);
-				}
+    // 循环接收客户端消息并处理
+    while (1) {
+        memset(msg, 0, sizeof(msg)); // 清空消息缓冲区
+        // 接收客户端发送的消息
+        if ((nrecvSize = read(i_connfd, msg, MAXSIZE)) < 0) {
+            printf("read Error: %s (errno: %d)\n", strerror(errno), errno);
+            continue;
+        } else if (nrecvSize == 0) { // 客户端关闭连接
+            printf("client has disconnected!\n");
+            close(i_connfd);
+            break;
+        } else {
+            printf("recvMsg:%s", msg);
+            // 将接收到的消息中的小写字母转换为大写字母
+            for (int i = 0; msg[i] != '\0'; i++) {
+                msg[i] = toupper(msg[i]);
+            }
+            // 将转换后的消息发送回客户端
+            if (write(i_connfd, msg, strlen(msg) + 1) < 0) {
+                printf("write Error: %s (errno: %d)\n", strerror(errno), errno);
+            }
+        }
+    }
 
-			}
-		}
-	}
-	close(i_connfd);
-	close(i_listenfd);
+    // 关闭连接套接字和监听套接字
+    close(i_connfd);
+    close(i_listenfd);
 
-	return 0;
+    return 0; // 程序正常退出
 }
